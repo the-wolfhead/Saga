d236baecd71c713a07afdd43d052c7e402673632
Daniel Otabor <danieldamianotabor@gmail.com>
2023-12-05 08:31:53 +0100

    Update Signup.js
    Adding boiler template for the signup page

--- Screens/Signup.js
+++ Screens/Signup.js
@@ -1,1 +1,21 @@
+// SignUpScreen.js
+
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import styles from './styles'; // Import the styles
+
+const SignUpScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Sign Up</Text>
+      {/* Add sign-up form or registration components here */}
+
+      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Home')}>
+        <Text style={styles.buttonText}>Sign Up</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default SignUpScreen;
 
